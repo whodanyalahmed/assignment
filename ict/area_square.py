3458af609839787e67e88f580da11f031dc07bf7
whodanyalahmed <Daniahmedkhatri@gmail.com>
2019-03-03 23:07:02 +0500

    with directories

---
+++ ict/area_square.py
@@ -0,0 +1,10 @@
+print("Area of a Square")
+print("----------------")
+
+l = input("Enter the length of the side of a square: ")
+l = int(l)
+
+A = l*l
+print("\n===================================")
+print("The area of the sqaure is " + str(A))
+print("===================================")
---
+++ ict/split_name.py
@@ -0,0 +1,12 @@
+print("Spliting name ")
+print("=============")
+
+name  = input("Enter your name: ")
+print(name.rsplit())
+print()
+print("\nSplited Name: ")
+print("--------------")
+
+for i in range(0,len(name)):
+    print(name[i]) 
+
